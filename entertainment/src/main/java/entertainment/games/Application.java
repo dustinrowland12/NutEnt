9a582bf1c21d4b4b91c030e233c98bac3a3d23dc
Dustin <Dustin@Dustin-HP>
2018-12-12 10:02:59 -0500

    Added support for postgreSQL in Heroku

--- entertainment/src/main/java/entertainment/games/Application.java
+++ entertainment/src/main/java/entertainment/games/Application.java
@@ -1,7 +1,15 @@
 package entertainment.games;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class Application {
@@ -9,5 +17,22 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
+	@Profile("test")
+    @Bean
+    public DataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.url(dbUrl);
+        dataSourceBuilder.username(username);
+        dataSourceBuilder.password(password);
+
+        return dataSourceBuilder.build();
+    }
 	
 }
